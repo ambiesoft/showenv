96412f26cb393fbd00af6b7abc95d701d8a40b26
ambiesoft <ambiesoft.trueff@gmail.com>
2021-07-16 15:57:53 +0900

    add create gitrev

---
+++ createdummygitrev.bat
@@ -0,0 +1,7 @@
+
+C:\local\python3\python.exe ..\distSolution\distSolution.py --show-dummygitrev-wchar > showenv\gitrev.h
+C:\local\python3\python.exe ..\distSolution\distSolution.py --show-dummygitrev-txt > showenv\gitrev
+
+
+:ERROR
+pause
